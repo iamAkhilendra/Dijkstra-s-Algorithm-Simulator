80bbaa4a51d4bf11bcebe9681cc8c0a0f80e3db8
iamAkhilendra <contactakhilendraojha@gmail.com>
2024-03-14 08:55:39 +0530

    imporved contact-me page css

--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -4,25 +4,27 @@ import './ContactMe.css';
 export default function ContactMe() {
     return (
         <div id='contact-me'>
-            <div id="contact-me-box">
-                <form action="">
-                    <div className='input-values' id="contact-me-user-name-div">
-                        <input type="text" name="user-name"/>
-                    </div>   
-                    <div className='input-values' id="contact-me-user-email-div">
-                        <input type="email" name="user-email"/>
-                    </div>     
-                    <div className='input-values' id="contact-me-user-phone-number-div">
-                        <input type="number" inputMode='numeric' pattern="[0-9]*" name="user-phone-number" />
-                    </div> 
-                    <div className='input-values' id="contact-me-form-message-div">
-                        <textarea name="user-message" cols="30" rows="10" id="contact-me-form-message" ></textarea>
-                    </div> 
-                    <div>
-                        <input type="submit" value="Submit" id="contact-me-submit-button" />
-                    </div>                
-                </form>
-            </div>
+            <form action="">
+                <div className="input-field">
+                    <input type="text" className="input-field-element"  placeholder=''/>
+                    <label>Name</label>
+                </div>
+                <div className="input-field">
+                    <input type="email" className="input-field-element"  placeholder=''/>
+                    <label>E-mail</label>
+                </div>
+                <div className="input-field">
+                    <input type="number" name="" id="" className="input-field-element"  placeholder=''/>
+                    <label>Phone Number</label>
+                </div>
+                <div className="input-field">
+                    <textarea name="" id="contact-me-message" cols="30" rows="10" placeholder=''></textarea>
+                    <label>Message</label>
+                </div>
+                <div>
+                    <input type="submit" value="Submit" id="contact-me-submit-button" />
+                </div>
+            </form>
         </div>
     )
 }
